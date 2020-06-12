02ef5f47b3edbede1991df540bab626d02128e12
Diana0604 <diana.vallverdu@gmail.com>
2020-06-12 19:04:36 +0100

    moving a bit

--- public/javascripts/tbl.js
+++ public/javascripts/tbl.js
@@ -1,13 +1,33 @@
 let myAudio = $("#audio")[0];
 
-$( "button" ).click(function() {
+$("#faxcontainer").hide();
+
+$( "#call" ).click(function() {
+    console.log('image clicked');
     myAudio.play();
     alert( "PLEASE LISTEN CAREFULLY" );
 });
 
-$("#clickcontainer").hide();
+
 
 myAudio.addEventListener("ended", function(){
     myAudio.currentTime = 0;
-    $("#clickcontainer").show();
+    $("#faxcontainer").show();
 });
+
+function moveCall() {
+    var elem = document.getElementById("call");
+    var pos = 0;
+    var id = setInterval(frame, 5);
+    function frame() {
+      if (pos == 600) {
+        clearInterval(id);
+      } else {
+        pos++;
+        elem.style.top = pos + 'px';
+        elem.style.left = pos + 'px';
+      }
+    }
+}
+
+moveCall();
