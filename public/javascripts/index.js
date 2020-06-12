970d81ddc4277fef0090401dee86a83defb11eca
Diana0604 <diana.vallverdu@gmail.com>
2020-06-12 12:50:56 +0100

    clean code

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,14 +1,8 @@
-console.log('hello world');
-
 let myAudio = $("#audio")[0];
 
 myAudio.addEventListener("ended", function(){
     myAudio.currentTime = 0;
     console.log('ended');
-    //$("#fax").toggle();
-    //$("#plug").toggle();
     $("#fax").show();
     $("#plug").show();
 });
-
-//$("#fax").toggle();
