c22f7f95367d475e9a3c36e48d3a7ea0577ab6eb
Diana0604 <diana.vallverdu@gmail.com>
2020-06-12 16:14:37 +0100

    basic tbl

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Fax Machine' });
 });
 
+router.get('/tbl', function(req, res, next){
+  res.render('tbl', {title: 'TBL'});
+});
+
 module.exports = router;
