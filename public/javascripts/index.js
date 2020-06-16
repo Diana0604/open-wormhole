14afa7adbbc8657e378e1e66aaa22b339ca3d00c
Diana0604 <diana.vallverdu@gmail.com>
2020-06-16 12:29:24 +0100

    open wormhole button grows

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,11 +1,19 @@
 let myAudio = $("#audio")[0];
 
-$("#fax").hide();
-$("#plug").hide();
+$(".hidden").hide();
 
 myAudio.addEventListener("ended", function(){
     myAudio.currentTime = 0;
     console.log('ended');
-    $("#fax").show();
-    $("#plug").show();
+    $(".hidden").show();
+    setInterval(increaseWidth, 50);
 });
+
+var posLeft = 470;
+var posBot = 270;
+function increaseWidth() {
+    $("#open-wormhole").width($("#open-wormhole").width() + 5);
+    posLeft -= 0.5;
+    posBot -= 0.5;
+    $("#open-wormhole").css({ 'left': posLeft + 'px', 'bottom': posBot + 'px'})
+}
