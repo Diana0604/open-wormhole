61e51dba2cfed213c560980c69c4da0f0774061c
Diana0604 <diana.vallverdu@gmail.com>
2020-06-16 16:55:34 +0100

    background music + womrhole button links

--- public/javascripts/connectivity.js
+++ public/javascripts/connectivity.js
@@ -3,6 +3,25 @@ console.log('connected');
 var password = document.getElementById("password");
 var form = document.getElementById("form");
 
+let backgroundAudio = $("#backgroundaudio")[0];
+
+let isPlaying = false;
+
+["click", "touchmove"].forEach((eventName)=>{
+  window.addEventListener(eventName, ()=>{
+    if(!isPlaying){
+      
+      try{
+        backgroundAudio.play()
+        isPlaying = true;
+      }catch(e){
+        console.warn(e.message);
+      }
+      
+    }
+  }); 
+});
+
 function validatePassword(){
   if(password.value != "2" && password.value != "two") {
 		event.preventDefault();
--- public/javascripts/security.js
+++ public/javascripts/security.js
@@ -1,8 +1,25 @@
-console.log('connected');
 
 counter = 0;
 
 let myAudio = $("#audio")[0];
+let backgroundAudio = $("#backgroundaudio")[0];
+
+let isPlaying = false;
+
+["click", "touchmove"].forEach((eventName)=>{
+  window.addEventListener(eventName, ()=>{
+    if(!isPlaying){
+      
+      try{
+        backgroundAudio.play()
+        isPlaying = true;
+      }catch(e){
+        console.warn(e.message);
+      }
+      
+    }
+  }); 
+});
 var password = document.getElementById("password");
 var form = document.getElementById("form");
 
