91c8a0fe674fd86a57d90e9957243e097ef83c3b
Diana0604 <diana.vallverdu@gmail.com>
2020-06-16 12:18:23 +0100

    password mistake

--- public/javascripts/connectivity.js
+++ public/javascripts/connectivity.js
@@ -4,14 +4,15 @@ var password = document.getElementById("password");
 var form = document.getElementById("form");
 
 function validatePassword(){
-  if(password.value != "2" || password.value != "two") {
+  if(password.value != "2" && password.value != "two") {
 		event.preventDefault();
 		return;
 	}
 }
 
 form.addEventListener("submit", function(event){
-    validatePassword();
+    validatePassword(
+    );
   });
 
 
